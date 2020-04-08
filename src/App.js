583088254d520cd089be33665712ba6055ad22da
Brian Sui <briansui2022@u.northwestern.edu>
2020-04-08 15:37:56 -0400

    finished fifth slice

--- src/App.js
+++ src/App.js
@@ -8,14 +8,53 @@ const Banner = ({title}) => (
   <Title>{title || '[loading...]' }</Title>
 )
 
-// CourseList component to map every course in courses object into its own HTML component
-const CourseList = ({courses}) => (
-  <Button.Group>
-    { courses.map(course => <Course key={course.id} course={course}/>)}
+const buttonColor = selected => (
+  // color is set to success green if it is selected
+  selected ? 'success' : null
+);
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const TermSelector = ({ state }) => (
+  // add hasAddons prop from rbx to have the buttons be connected 
+  <Button.Group hasAddons> 
+    { Object.values(terms).map(value => // Object.values(terms) gets all the values of terms global variable
+      <Button key={value}
+        color = { buttonColor(value===state.term) }
+        onClick = { () => state.setTerm(value) }
+        >
+        {value}
+      </Button>
+      )
+    }
   </Button.Group>
 )
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const useSelection = () =>{
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y=> y!==x) : [x].concat(selected))
+  };
+  return [selected, toggle];
+}
+
+// CourseList component to map every course in courses object into its own HTML component
+const CourseList = ({ courses }) =>{
+  const [term, setTerm] = useState('Fall'); // initialize state to fall term
+  const [selected, toggle] = useSelection();
+  const termCourses = courses.filter(course => term === getCourseTerm(course)); // filter courses by term
+
+  // pass in term as props to TermSelector
+  // state={ {term, setTerm}} is same as { term: term, setTerm: setTerm }
+  return(
+  <React.Fragment>
+    <TermSelector state={ {term, setTerm}}/> 
+    <Button.Group>
+      { termCourses.map(course => <Course key={ course.id } course={ course } state= { {selected, toggle} } /> )}
+    </Button.Group>
+  </React.Fragment>
+  );
+};
 
 // getting the first character of the course in course objects
 const getCourseTerm = course => (
@@ -27,11 +66,60 @@ const getCourseNumber = course => (
   course.id.slice(1,4)
 );
 
-const Course = ({ course }) => (
-  <Button>
+const hasConflict = (course,selected) => (
+  selected.some(selection=>courseConflict(course,selection))
+);
+
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+
+const Course = ({ course, state }) => (
+  <Button color={ buttonColor(state.selected.includes(course)) }
+    onClick={ () => state.toggle(course) }
+    disabled={ hasConflict(course, state.selected )}>
     { getCourseTerm(course) } CS { getCourseNumber(course) } : { course.title }
   </Button>
 )
+
+const daysOverlap = (days1, days2) => (
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
 
 
 const App = () => {
@@ -45,7 +133,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response; 
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, []);
